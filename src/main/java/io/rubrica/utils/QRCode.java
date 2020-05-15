2ed93c3f60bfddad5eae8ac76c91e7b202e4193a
Misael Fernández <misael.fernandez@mintel.gob.ec>
2020-05-15 18:54:17 -0500

    V1.0.1-BETA-7 Generar QR con información adicional

--- src/main/java/io/rubrica/utils/QRCode.java
+++ src/main/java/io/rubrica/utils/QRCode.java
@@ -35,6 +35,7 @@ public class QRCode {
         String text = "Nombre firmante: MISAEL VLADIMIR FERNANDEZ CORREA\n"
                 + "Razón: Firmado digitalmente con RUBRICA\n" + "Fecha firmado: 2018-05-31T11:39:47.247-05:00\n"
                 + "Firmado digitalmente con RUBRICA\n" + "https://minka.gob.ec/rubrica/rubrica";
+//        String text = "";
 
         try {
 
